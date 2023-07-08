b6a0be94ce2bc1e04d3bbf616578df1bd1316403
Nagaraj1007 <nagaraj.20it@sonatech.ac.in>
2023-07-08 12:59:05 +0530

    Initial commit done

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        mangoTreeOrNot object =new mangoTreeOrNot();
+        System.out.println(object.mangoOrNot(10,60,2));
+    }
+}
