6bc99ed7f4f2a363bdeba389e38e0581ecb7b0f9
Nagaraj1007 <nagaraj.20it@sonatech.ac.in>
2023-07-09 18:59:29 +0530

    added some code extra

--- src/mangoTreeOrNot.java
+++ src/mangoTreeOrNot.java
@@ -1,15 +1,19 @@
 public class mangoTreeOrNot {
     public boolean mangoOrNot(int row , int col , int number){
-        if(number >= 1 && number <= row){
-            return true;
-        }
-        if (number > (row * col) - col && number <= row * col ) {
+        if(number >= 1 && number <= col){
             return true;
         }
         int firstColumnNumber = 1;
         for(int iterator = 1 ; iterator < row ;iterator++){
             firstColumnNumber += col;
             if (firstColumnNumber == number){
+                return true;
+            }
+        }
+        int lastColumnNumber = col;
+        for (int iterator = 1 ; iterator < row ; iterator++){
+            lastColumnNumber += col;
+            if (lastColumnNumber == number){
                 return true;
             }
         }
